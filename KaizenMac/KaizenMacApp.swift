b8726ef32bce2a330151a7ef9d164ee92b41290f
Dante <motiv8lab@gmail.com>
2024-05-24 12:53:29 -0400

    Initial Commit

---
+++ KaizenMac/KaizenMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenMacApp.swift
+//  KaizenMac
+//
+//  Created by Dante Kim on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct KaizenMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
